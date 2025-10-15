74dc7726d46e7d8a53bf355a7219f31e71930506
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2025-10-16 06:43:53 +0800

    fix: offset should be prepared before object start

--- generator/marshaler_generator.go
+++ generator/marshaler_generator.go
@@ -35,60 +35,114 @@ func generateMarshalMessage(f *File, def parser.Definition, modelName string, wi
 				),
 			)
 		}
-		// <FlatBufferMessage>Start(b)
-		g.Id(def.Name + "Start").Call(Id("b"))
-		// Start adding codes to unmarshal the struct fields.
+
+		// Pre-create all strings and nested objects BEFORE calling Start
+		offsetVarIndex := 0
 		for _, field := range def.Fields {
 			fieldAccessor := Id("t").Dot(toExportedName(field.Name))
-			if field.IsVector {
-				fieldLength := Id("len").Call(fieldAccessor)
-				// <FlatBufferMessage>Start<FieldName>Vector(b, len(t.FieldName))
-				g.Id(def.Name+"Start"+toExportedName(field.Name)+"Vector").Call(Id("b"), fieldLength)
-				g.For(Id("i").Op(":=").Range().Add(fieldLength)).BlockFunc(func(g *Group) {
-					// If the field is a nested struct, we need to marshal it first.
-					indexedFieldAccessor := fieldAccessor.Clone().Index(fieldLength.Clone().Op("-").Id("i").Op("-").Lit(1))
-					if field.IsNested() {
-						// b.PrependUOffsetT(t.FieldName[i].MarshalModel(b))
-						g.Comment("The array should be reversed.")
-						if !withoutDecryption {
-							indexedFieldAccessor.Dot("FlatBuffer").Dot("InitKey").Call(Id("t").Dot("FlatBuffer").Dot("TableKey"))
-						}
-						g.Id("b").Dot("PrependUOffsetT").Call(indexedFieldAccessor.Dot("MarshalModel").Call(Id("b")))
-						return
-					}
+			offsetVarName := "__offset_" + field.Name
 
-					// b.Prepend<GoType>(fbsutils.Convert(t.FieldName[i], t.FlatBuffer.TableKey))
-					goType := strcase.ToCamel(getBaseGoType(field).GoString())
-					switch {
-					case field.IsString:
-						goType = "UOffsetT"
-						indexedFieldAccessor = Id("b").Dot("CreateString").Call(indexedFieldAccessor)
-					case field.IsEnum:
-						goType = "Int32"
-						indexedFieldAccessor = fieldConverter(Id("int32").Call(indexedFieldAccessor))
-					case field.IsPrimitive() && field.Type != "bool":
-						indexedFieldAccessor = fieldConverter(indexedFieldAccessor)
-					default:
-						goType = strcase.ToCamel(getBaseGoType(field).GoString())
-					}
-					g.Id("b").Dot("Prepend" + goType).Call(indexedFieldAccessor)
-				})
-				// <FlatBufferMessage>Add<FieldName>(b, b.EndVector(len(t.FieldName)))
-				fieldSetter(g, field, Id("b").Dot("EndVector").Call(Id("len").Call(fieldAccessor)))
-			} else if field.IsNested() {
-				// <FlatBufferMessage>Add<FieldName>(b, <fieldAccessor>.MarshalModel(b))
-				// Call InitKey with current table key if the field is a nested struct.
-				if !withoutDecryption {
-					fieldAccessor.Dot("FlatBuffer").Dot("InitKey").Call(Id("t").Dot("FlatBuffer").Dot("TableKey"))
+			if field.IsVector {
+				if field.IsString || field.IsNested() {
+					// Create offset variable for vector of strings or nested objects
+					g.Var().Id(offsetVarName).Qual("github.com/google/flatbuffers/go", "UOffsetT")
 				}
-				fieldSetter(g, field, fieldAccessor.Dot("MarshalModel").Call(Id("b")))
-			} else {
+
 				if field.IsString {
-					fieldAccessor = Id("b").Dot("CreateString").Call(fieldConverter(fieldAccessor))
-				} else if field.Type != "bool" {
+					// Pre-create vector of strings
+					fieldLength := Id("len").Call(fieldAccessor)
+					g.Id("__stringOffsets_"+field.Name).Op(":=").Make(Index().Qual("github.com/google/flatbuffers/go", "UOffsetT"), fieldLength)
+					g.For(Id("i").Op(":=").Range().Add(fieldLength)).Block(
+						Id("__stringOffsets_" + field.Name).Index(Id("i")).Op("=").Id("b").Dot("CreateString").Call(
+							fieldConverter(fieldAccessor.Clone().Index(Id("i"))),
+						),
+					)
+					// Create the vector
+					g.Id(def.Name+"Start"+toExportedName(field.Name)+"Vector").Call(Id("b"), fieldLength)
+					g.For(Id("i").Op(":=").Range().Add(fieldLength)).Block(
+						Id("b").Dot("PrependUOffsetT").Call(
+							Id("__stringOffsets_" + field.Name).Index(fieldLength.Clone().Op("-").Id("i").Op("-").Lit(1)),
+						),
+					)
+					g.Id(offsetVarName).Op("=").Id("b").Dot("EndVector").Call(fieldLength)
+					offsetVarIndex++
+				} else if field.IsNested() {
+					// Pre-create vector of nested objects
+					fieldLength := Id("len").Call(fieldAccessor)
+					g.Id("__nestedOffsets_"+field.Name).Op(":=").Make(Index().Qual("github.com/google/flatbuffers/go", "UOffsetT"), fieldLength)
+					g.For(Id("i").Op(":=").Range().Add(fieldLength)).BlockFunc(func(g *Group) {
+						indexedFieldAccessor := fieldAccessor.Clone().Index(Id("i"))
+						if !withoutDecryption {
+							g.Add(indexedFieldAccessor.Clone().Dot("InitKey").Call(Id("t").Dot("FlatBuffer").Dot("TableKey")))
+						}
+						g.Id("__nestedOffsets_" + field.Name).Index(Id("i")).Op("=").Add(indexedFieldAccessor.Clone().Dot("MarshalModel").Call(Id("b")))
+					})
+					// Create the vector (in reverse order)
+					g.Id(def.Name+"Start"+toExportedName(field.Name)+"Vector").Call(Id("b"), fieldLength)
+					g.For(Id("i").Op(":=").Range().Add(fieldLength)).Block(
+						Id("b").Dot("PrependUOffsetT").Call(
+							Id("__nestedOffsets_" + field.Name).Index(fieldLength.Clone().Op("-").Id("i").Op("-").Lit(1)),
+						),
+					)
+					g.Id(offsetVarName).Op("=").Id("b").Dot("EndVector").Call(fieldLength)
+					offsetVarIndex++
+				}
+			} else if field.IsNested() {
+				// Pre-create single nested object
+				if !withoutDecryption {
+					g.Add(fieldAccessor.Clone().Dot("InitKey").Call(Id("t").Dot("FlatBuffer").Dot("TableKey")))
+				}
+				g.Id(offsetVarName).Op(":=").Add(fieldAccessor.Clone().Dot("MarshalModel").Call(Id("b")))
+				offsetVarIndex++
+			} else if field.IsString {
+				// Pre-create single string
+				g.Id(offsetVarName).Op(":=").Id("b").Dot("CreateString").Call(fieldConverter(fieldAccessor))
+				offsetVarIndex++
+			}
+		}
+
+		// <FlatBufferMessage>Start(b)
+		g.Id(def.Name + "Start").Call(Id("b"))
+
+		// Start adding codes to marshal the struct fields.
+		for _, field := range def.Fields {
+			fieldAccessor := Id("t").Dot(toExportedName(field.Name))
+			offsetVarName := "__offset_" + field.Name
+
+			if field.IsVector {
+				if field.IsString || field.IsNested() {
+					// Use pre-created offset
+					fieldSetter(g, field, Id(offsetVarName))
+				} else {
+					// Handle primitive vectors inline
+					fieldLength := Id("len").Call(fieldAccessor)
+					g.Id(def.Name+"Start"+toExportedName(field.Name)+"Vector").Call(Id("b"), fieldLength)
+					g.For(Id("i").Op(":=").Range().Add(fieldLength)).BlockFunc(func(g *Group) {
+						indexedFieldAccessor := fieldAccessor.Clone().Index(fieldLength.Clone().Op("-").Id("i").Op("-").Lit(1))
+						goType := strcase.ToCamel(getBaseGoType(field).GoString())
+
+						if field.IsEnum {
+							goType = "Int32"
+							indexedFieldAccessor = fieldConverter(Id("int32").Call(indexedFieldAccessor))
+						} else if field.IsPrimitive() && field.Type != "bool" {
+							indexedFieldAccessor = fieldConverter(indexedFieldAccessor)
+						}
+
+						g.Id("b").Dot("Prepend" + goType).Call(indexedFieldAccessor)
+					})
+					fieldSetter(g, field, Id("b").Dot("EndVector").Call(fieldLength))
+				}
+			} else if field.IsNested() {
+				// Use pre-created nested object offset
+				fieldSetter(g, field, Id(offsetVarName))
+			} else if field.IsString {
+				// Use pre-created string offset
+				fieldSetter(g, field, Id(offsetVarName))
+			} else {
+				// Handle primitive fields inline
+				if field.Type != "bool" {
 					fieldAccessor = fieldConverter(fieldAccessor)
 				}
-				// <FlatBufferMessage>Add<FieldName>(b, fbsutils.Convert(<FieldName>, t.FlatBuffer.TableKey))
 				fieldSetter(g, field, fieldAccessor)
 			}
 		}
